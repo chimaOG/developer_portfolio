8c1796b1adc9f91d017950df065df9354a3a740a
Samuel Ogbonna <chimaogbonna30@gmail.com>
2022-04-30 16:41:41 +0100

    Added code to make the projects more dynamic

--- src/components/LeftProject.js
+++ src/components/LeftProject.js
@@ -3,12 +3,12 @@ import {FiGithub} from 'react-icons/fi';
 import {HiExternalLink} from 'react-icons/hi';
 
 
-const LeftProject = () => {
+const LeftProject = ({projectDetails : {name, technologies, description, gitLink, projLink, imgLink}}) => {
     return (
         <div className = 'relative min-h-min'>
             <div className = {`relative w-full`}>
                 <a href = 'https://paulstar.herokuapp.com/' target='blank' >
-                    <img src={'https://res.cloudinary.com/dhdzo61wo/image/upload/v1645479936/Screenshot_2022-02-21_at_10.43.20_PM_zdwgsc.png'} 
+                    <img src={imgLink} 
                     className = ' relative w-full h-80 md:h-96 md:w-7/12  rounded-lg'  
                    />
 
@@ -18,24 +18,24 @@ const LeftProject = () => {
             </div>
             <div className = 'absolute top-0 right-0 p-5 md:p-0 w-full md:w-1/2   h-80 md:h-full flex flex-col items-start md:items-end bg-slate-900/75 md:bg-transparent'>
                 <p className = 'text-light_green text-base md:mt-5'>Featured Project</p>
-                <a href = 'https://paulstar.herokuapp.com/' target='blank' >
-                     <p className = 'md:mt-2 text-title_colors text-xl md:text-3xl hover:text-light_green'>Photography Website</p> 
+                <a href = {projLink} target='blank' >
+                     <p className = 'md:mt-2 text-title_colors text-xl md:text-3xl hover:text-light_green'>{name}</p> 
                 </a>
                 <div className = 'md:mt-7 rounded-lg py-2 md:p-5 md:bg-tab_bg text-banner_grey text-sm md:text-base'>
                     <p className = 'md:ml-3'>
-                    A website for a photography business to interact with customers. 
-                    It gives potential customers the ability to view the portfolio and contact the business for potential deals.
+                        {description}
                     </p>
                 </div>
                 <div className = 'mt-2 md:mt-5 flex justify-end items-center text-banner_grey text-sm md:text-base'>
-                    <span className = ''>React</span>
-                    <span className = 'ml-4'>TailwindCSS</span>
-                    <span className = 'ml-4'>AnimateCSS</span>
-                    <span className = 'ml-4'>Heroku</span>
+                {
+                        technologies.map((item) => (
+                            <span className = 'ml-4' key = {item}>{item}</span>
+                    ))
+                    }
                 </div>
                 <div className = 'mt-2 md:mt-3 flex font-bold text-xl md:text-2xl text-banner_grey'>
-                    <a href = 'https://github.com/chimaOG/paulstart.git' target='blank'> <FiGithub className='hover:text-light_green' />  </a>
-                    <a href = 'https://paulstar.herokuapp.com/' target='blank'> <HiExternalLink className=' ml-5 hover:text-light_green' />  </a>
+                    <a href = {gitLink} target='blank'> <FiGithub className='hover:text-light_green' />  </a>
+                    <a href = {projLink} target='blank'> <HiExternalLink className=' ml-5 hover:text-light_green' />  </a>
                 </div>
             </div>
         </div>
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,6 +1,29 @@
 import React from 'react';
 
 import LeftProject from './LeftProject';
+import RightProject from './RightProject';
+
+const projectData = [
+    {
+        name: 'ReactMeals App',
+        description: "An e-commerce websites where customers can view all the available meals, get pricing information, add them to cart and eventually place an order. ",
+        technologies: ['React', 'TailwindCSS', 'FireBase', 'Heroku', 'Context'],
+        gitLink: 'https://github.com/chimaOG/React-Meals-Project.git',
+        projLink: 'https://reactmealscart.herokuapp.com/',
+        imgLink: 'https://res.cloudinary.com/dhdzo61wo/image/upload/v1651332317/Screenshot_2022-04-30_at_4.23.57_PM_ddhtac.png'
+    },
+    {
+        name: 'Photography Website',
+        description: 'A website for a photography business to interact with customers. It gives potential customers the ability to view the portfolio and contact the business for potential deals.',
+        technologies: ['React', 'TailwindCSS', 'AnimateCSS', 'Heroku'],
+        gitLink: 'https://github.com/chimaOG/paulstart.git',
+        projLink: 'https://paulstar.herokuapp.com/',
+        imgLink: 'https://res.cloudinary.com/dhdzo61wo/image/upload/v1645479936/Screenshot_2022-02-21_at_10.43.20_PM_zdwgsc.png'
+    },
+    
+    
+]
+
 
 const Project = () => {
     return (
@@ -12,8 +35,13 @@ const Project = () => {
                 <div className = 'h-px w-52 bg-text_dark_grey ml-5' style={{backgroundColor:'#233554'}}>
                 </div>
             </div>
+
             <div className = 'mt-20'>
-                <LeftProject />
+                <LeftProject projectDetails = {projectData[0]}  />
+            </div>
+
+            <div className = 'mt-20'>
+                <RightProject projectDetails = {projectData[1]} />
             </div>
         </div>
     )
